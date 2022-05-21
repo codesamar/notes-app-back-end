dede9f757ec55757f8e23cd1caed4709159ce850
RomadloniP <romadloni86@gmail.com>
2022-05-21 21:45:01 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,7 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable spaced-comment */
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+const notes = [];
+
+module.exports = notes;
